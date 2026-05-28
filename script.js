6cb13d1e65e387e6fc858032e84d0aa5df293f32
llxmxx <tanishkac1309@gmail.com>
2026-05-28 23:08:45 +0530

    4 presets to select from. wayyy better now. v fun.

--- script.js
+++ script.js
@@ -2,40 +2,51 @@ const upload = document.getElementById("upload");
 const canvas = document.getElementById("canvas");
 const con = canvas.getContext("2d");
 const asciiout = document.getElementById("ascii");
-const chars = "@#W$9876543210?!abc;:+=-_,. ";
+const presets = {normal: "@%#&$MW8B0XKRqpZUdhmvyc*+=~-_;:,. ", simple: "@%#*+=-:. ", block: "█▓▒░ ", minimal: "....    "};
+let presetsel = document.getElementById("preset");
+let img = null;
 
 upload.addEventListener("change", () => {
     const file = upload.files[0];
     if(!file) return;
 
-    const img = new Image();
-    img.src = URL.createObjectURL(file);
-    img.onload = () => {
-        const width = 120;
-        const scale = img.height/img.width;
-
-        canvas.width = width;
-        canvas.height = width*scale*0.5;
-        con.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-        const imageData = con.getImageData(0,0, canvas.width, canvas.height);
-        const pixels = imageData.data;
-
-        let ascii="";
-
-        for(let i = 0; i < canvas.height; i++){
-            for(let j = 0; j < canvas.width; j++){
-                const index = (i*canvas.width + j)*4;
-                const r = pixels[index];
-                const g = pixels[index + 1];
-                const b = pixels[index + 2];
-
-                const brightness = 0.299*r + 0.587*g + 0.114*b;
-                const charIndex = Math.floor((brightness/255)*(chars.length-1));
-                ascii += chars[charIndex];
-            }
-            ascii += '\n';
-        }
-        asciiout.textContent = ascii;
+    const currimg = new Image();
+    currimg.src = URL.createObjectURL(file);
+    currimg.onload = () => {
+        img = currimg;
+        convertascii();
     };
 });
+
+presetsel.addEventListener("change", () => {
+    if(img) convertascii()
+})
+
+function convertascii() {
+    const chars = presets[presetsel.value];
+    const width = 120;
+    const scale = img.height/img.width;
+
+    canvas.width = width;
+    canvas.height = width*scale*0.5;
+    con.drawImage(img, 0, 0, canvas.width, canvas.height);
+    const imageData = con.getImageData(0,0, canvas.width, canvas.height);
+    const pixels = imageData.data;
+
+    let ascii="";
+
+    for(let i = 0; i < canvas.height; i++){
+        for(let j = 0; j < canvas.width; j++){
+            const index = (i*canvas.width + j)*4;
+            const r = pixels[index];
+            const g = pixels[index + 1];
+            const b = pixels[index + 2];
+
+            const brightness = 0.299*r + 0.587*g + 0.114*b;
+            const charIndex = Math.floor((brightness/255)*(chars.length-1));
+            ascii += chars[charIndex];
+        }
+        ascii += '\n';
+    }
+    asciiout.textContent = ascii;
+}
